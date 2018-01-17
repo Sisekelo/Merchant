cdde10449d511853909f7ca674501b01a9aae0fb
Sisekelo <sidlamini15@alustudent.com>
2018-01-17 15:58:55 +0200

    a

--- auth.php
+++ auth.php
@@ -1,4 +1,11 @@
 <?php
 ob_start();
-
-	?>
+	//Start session
+	session_start();
+	
+	//Check whether the session variable SESS_MEMBER_ID is present or not
+	if(!isset($_SESSION['SESS_MEMBER_ID']) || (trim($_SESSION['SESS_MEMBER_ID']) == '')) {
+		header("location: admin_index.php");
+		exit();
+	}
+?>
