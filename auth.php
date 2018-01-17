3080a64d73088cbb2833b62e4dd4c48ac413b1f9
Sisekelo <sidlamini15@alustudent.com>
2018-01-17 15:33:44 +0200

    a

--- auth.php
+++ auth.php
@@ -3,7 +3,7 @@ ob_start();
 	
 	//Check whether the session variable SESS_MEMBER_ID is present or not
 	if(!isset($_SESSION['SESS_MEMBER_ID']) || (trim($_SESSION['SESS_MEMBER_ID']) == '')) {
-		header("location: admin_index.php");
+		header("location: index.php");
 		exit();
 	}
 ?>
