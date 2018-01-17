1d2a06b4a82058fce00bae6523155439c531d91c
Sisekelo <sidlamini15@alustudent.com>
2018-01-17 15:32:19 +0200

    a

--- auth.php
+++ auth.php
@@ -1,7 +1,5 @@
 <?php
 ob_start();
-	//Start session
-	session_start();
 	
 	//Check whether the session variable SESS_MEMBER_ID is present or not
 	if(!isset($_SESSION['SESS_MEMBER_ID']) || (trim($_SESSION['SESS_MEMBER_ID']) == '')) {
