c68fc707b3de014def01ce4ef059f7584b75f3c6
Sisekelo <sidlamini15@alustudent.com>
2018-01-23 11:10:52 +0400

    a

--- pickUpboom.php
+++ pickUpboom.php
@@ -49,7 +49,7 @@
 <!-- MENU START -->
 <div id="menu">
 	<ul class="group" id="menu_group_main">
-		<li class="item first" id="one"><a href="boomgate.php" class="main "><span class="outer"><span class="inner dashboard">Confirmations</span></span></a></li>
+		<li class="item first" id="one"><a href="boomgate.php?vendor=BoomGate" class="main "><span class="outer"><span class="inner dashboard">Confirmations</span></span></a></li>
 		<li class="item middle" id="four"><a href="preparationboom.php" class="main "><span class="outer"><span class="inner media_library">Preparations</span></span></a></li> 
 		<li class="item middle" id="eight"><a href="pickUpboom.php" class="main current"><span class="outer"><span class="inner users">Pick Ups</span></span></a></li>  
 		<li class="item last" id="eight"><a href="deliveriesboom.php" class="main"><span class="outer"><span class="inner settings">Deliveries</span></span></a></li>        
